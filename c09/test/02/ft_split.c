c99ba1a78cf7b17eee79e20ea3e27972cc4c62b5
Nicolás <85840576+onicolasm@users.noreply.github.com>
2022-08-30 23:24:01 +0200

    Add files via upload

---
+++ c09/test/02/ft_split.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int lenght(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+        i++;
+    return i;
+}
+
+char *copy(char *str, int pos)
+{
+    char *tab;
+    int i = 0;
+    int index = 0;
+
+    index = index + pos;
+
+    tab = malloc(sizeof(char*) * (pos + 1));
+    while( i < pos)
+    {
+        tab[i] = str[i];
+        i++;
+    }
+    tab[i] = '\0';
+    return tab;
+}
+
+char **ft_split(char *str, char *charset)
+{
+    char **split;
+    int i;
+    int i2;
+    int j;
+    int index = 0;
+
+    //if(!str || !charset)
+     //   return 0;
+    
+    split = malloc(sizeof(char *) * (lenght(str) + 1));
+    if(!split)
+        return 0;
+    i = 0;
+    j = 0;
+    while(str[i + index])
+    {
+        i2 = 0;
+        while(charset[i2])
+        {
+            if(str[i] == charset[i2] || str[i] == '\0')
+            {   
+
+                split[j] = copy(str, i);
+                index = i;
+                j++;
+            }
+            i2++;
+        }
+        i++;
+    }
+    split[j] = 0;
+    return split;
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    char **a;
+
+    i = 0;
+    a = ft_split(argv[1], argv[2]);
+    while (a[i])
+    {
+        printf("the str:%s\n", a[i]);
+        i++;
+    }
+}
