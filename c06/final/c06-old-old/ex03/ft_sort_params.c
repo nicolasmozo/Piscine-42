d694f73a63870ffd8edfe669b1cdf54a661f3a97
Nicolás <85840576+onicolasm@users.noreply.github.com>
2022-08-30 22:39:43 +0200

    Add files via upload

---
+++ c06/final/c06-old-old/ex03/ft_sort_params.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omozo-av <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 19:45:31 by omozo-av          #+#    #+#             */
+/*   Updated: 2022/08/24 20:52:04 by omozo-av         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	ft_putchar('\n');
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*temp;
+	int		i;
+	int		loop;
+
+	
+	loop = 1;
+	while (loop)
+	{
+		loop = 0;
+		i = 0;
+		while (++i < argc -1)
+		{
+			if ((ft_strcmp(argv[i], argv[i + 1])) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = temp;
+				loop = 1;
+			}
+		}
+	}
+	i = 0;
+	while (++i < argc)
+	{
+		ft_putstr(argv[i]);
+	}
+}
---
+++ c06/final/ex01/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omozo-av <omozo-av@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 19:45:31 by omozo-av          #+#    #+#             */
+/*   Updated: 2022/08/25 18:08:54 by omozo-av         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		i++;
+	}
+}
